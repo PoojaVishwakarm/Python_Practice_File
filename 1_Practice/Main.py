348f3a5ce01aa60281d4188983e1ffb01a1ade66
PoojaVishwakarm <poojavishwakarma7286@gmail.com>
2025-12-01 16:00:47 +0530

    final

--- 1_Practice/Main.py
+++
@@ -1,6 +0,0 @@
-def calculate_triangle_area(base,height):
-    print("values of __name__:",__name__)
-    return(base*height)/2
-
-if __name__=="__main__":
-    print("Area:", calculate_triangle_area(10,5))
